7a5e4a698b95741c8783f9c495fc9a706e21d8e8
Mohammad Abdellahi <mohi.abdellahi.com>
2024-01-03 15:11:40 +0100

    Initial Commit

---
+++ NFGGalleryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NFGGalleryApp
+//
+//  Created by Mohammad Abdellahi (Speed4Trade GmbH) on 03.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
